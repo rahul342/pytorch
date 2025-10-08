74620aa8faec26a9fda19ccb34cec9172e3eb443
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-10-08 22:40:47 +0800

    [pytree] add `treespec_{leaf,tuple,dict}` functions for args_spec modification
    ghstack-source-id: b1cfb7b2e8d05475bb121b5e5e62668fc826fa17
    Pull-Request: https://github.com/pytorch/pytorch/pull/160843

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -468,7 +468,7 @@ def _unlift_graph(
         gm,
         lifted_inputs,
         mutated_outputs,
-        pytree.LeafSpec(),
+        pytree.treespec_leaf(),
         None,
     )
     return unlifted_gm
